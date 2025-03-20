ad1325dfcddf5da9282995c188abc431bbf8f267
s33857 <s33857@pjwstk.edu.pl>
2025-03-20 11:24:20 +0100

    .

--- src/Klient.java
+++ src/Klient.java
@@ -1,2 +1,61 @@
 public class Klient {
+    private int id;
+    private String imie;
+    private String nazwisko;
+    private String email;
+    private boolean czyStaly;
+
+    public Klient(int id, String imie, String nazwisko, String email, boolean czyStaly) {
+        this.id = id;
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.email = email;
+        this.czyStaly = czyStaly;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public boolean isCzyStaly() {
+        return czyStaly;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setCzyStaly(boolean czyStaly) {
+        this.czyStaly = czyStaly;
+    }
+
+    public void wyswietlInformacje() {
+        String statusKlienta = czyStaly ? "stały klient (otrzymuje zniżki)" : "klient niezarejestrowany";
+        System.out.println("ID: " + id + ", Imię: " + imie + ", Nazwisko: " + nazwisko +
+                ", Email: " + email + ", Status: " + statusKlienta);
+    }
 }
--- src/Produkt.java
+++ src/Produkt.java
@@ -1,2 +1,60 @@
 public class Produkt {
+    private int id;
+    private String nazwa;
+    private String kategoria;
+    private double cena;
+    private int iloscWMagazynie;
+
+    public Produkt(int id, String nazwa, String kategoria, double cena, int iloscWMagazynie) {
+        this.id = id;
+        this.nazwa = nazwa;
+        this.kategoria = kategoria;
+        this.cena = cena;
+        this.iloscWMagazynie = iloscWMagazynie;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNazwa() {
+        return nazwa;
+    }
+
+    public String getKategoria() {
+        return kategoria;
+    }
+
+    public double getCena() {
+        return cena;
+    }
+
+    public int getIloscWMagazynie() {
+        return iloscWMagazynie;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNazwa(String nazwa) {
+        this.nazwa = nazwa;
+    }
+
+    public void setKategoria(String kategoria) {
+        this.kategoria = kategoria;
+    }
+
+    public void setCena(double cena) {
+        this.cena = cena;
+    }
+
+    public void setIloscWMagazynie(int iloscWMagazynie) {
+        this.iloscWMagazynie = iloscWMagazynie;
+    }
+
+    public void wyswietlInformacje() {
+        System.out.println("ID: " + id + ", Nazwa: " + nazwa + ", Kategoria: " + kategoria +
+                ", Cena: " + cena + " PLN, Ilość w magazynie: " + iloscWMagazynie + " szt.");
+    }
 }
